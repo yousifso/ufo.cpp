13b84fd3b05185d9bd92348266e4b011d98d4c9a
Yousifso <99320950+yousifso@users.noreply.github.com>
2024-01-31 12:56:38 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "ufo_functions.hpp"
+
+int main() {
+  std::string codeword = "codecademy";
+  std::string answer = "----------";
+  int misses = 0;
+  greet();
+  std::vector<char> incorrect;
+  bool guess = false;
+  char letter;
+  while (answer != codeword && misses < 7) {
+    display_misses(misses);
+    display_status(incorrect, answer);
+    misses++;
+
+    std::cout << "\nPlease enter your guess: \n";
+    std::cin >> letter;
+    for (int i = 0; i < codeword.length(); i++) {
+      if (letter == codeword[i]) {
+        answer[i] = letter;
+        guess = true;
+        }
+    }
+    if (guess) {
+      std::cout << "\ncorrect! You are closer to cracking the codeword.\n";
+    }
+    else {
+      std::cout << "\nIncorrect! The tractor beam pulls the person in further.\n";
+      incorrect.push_back(letter);
+      misses++;
+    }
+  }
+end_game(answer, codeword);
+guess = false;
+}
