9aa7a769411629ad1f7868a6b9f1c69bb2468031
Yousifso <99320950+yousifso@users.noreply.github.com>
2024-01-31 12:58:48 +0100

    Update ufo.hpp

--- ufo.hpp
+++ ufo.hpp
@@ -1,8 +1,7 @@
 #include <vector>
 
-void display_misses(int misses);
-
 // Declare functions
+void display_misses(int misses);
 void greet();
 void end_game(std::string answer, std::string codeword);
 void display_status(std::vector<char> incorrect, std::string answer);
